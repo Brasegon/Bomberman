dca07d66c05dff7b2fefd2b9c14e1d3b705d0109
brandon.segers@epitech.eu <brandon.segers@epitech.eu>
2020-05-27 12:08:16 +0200

    ajout CMake + outil pour clear Cmake

---
+++ clear.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Suppression des fichiers inutile CMake"
+rm -f Makefile
+rm -rf CMakeFiles
+rm -f CMakeCache.txt
+rm -rf cmake_install.cmake
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_indie_studio_2019 [WSL: Debian]
+** File description:
+** main
+*/
+
+int main()
+{
+    return (0);
+}
